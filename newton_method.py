05d47f2a8fb51e5c6d116da971c123baa6517b94
yctok <ychuang@wm.edu>
2023-01-03 13:38:57 -0500

    Create newton_method.py

---
+++ newton_method.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Dec 31 22:10:13 2022
+
+@author: user
+"""
+
+"""
+The goal:
+    find the sqare root of a number using Newton's method with argparse
+
+"""
+
+import math
+import argparse
+
+#find sqare root of x
+#g the initial guess
+
+g_ar = np.zeros(2)
+g_ar = 
+
+if 
