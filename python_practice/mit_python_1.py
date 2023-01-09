be0598272d13b5525377d4e48014ff30ea712b86
yctok <ychuang@wm.edu>
2023-01-08 19:41:02 -0500

    Create mit_python_1.py

---
+++ python_practice/mit_python_1.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec  8 00:12:26 2022
+
+@author: user
+"""
+
+"""
+The lecturer wants to demonstate what are imperative and declarative knowledge.
+Declarative knowledge:
+    statement of a goal
+Imperative knowledge:
+    a recipe to realize the goal
+
+Example:
+    find a random integer between 16 and 272.
+
+The following code demonstate how to find a random integer between 16 and 272
+
+"""
+
+import random
+rand_a = random.randint(16, 272)
+print(rand_a)
+
+
+
+
+
