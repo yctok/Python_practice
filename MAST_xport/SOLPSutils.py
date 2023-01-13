44c6c7908cc0d6755870e8b854d93291afd4a262
yctok <ychuang@wm.edu>
2023-01-13 09:41:57 -0500

    Update SOLPSutils.py

--- MAST_xport/SOLPSutils.py
+++ MAST_xport/SOLPSutils.py
@@ -380,6 +380,37 @@ def getProfDBPedFit(shotnum, timeid, runid, write_to_file=None):
 
 # ----------------------------------------------------------------------------------------
 
+"""
+read .dat file from MAST 
+"""
+
+def read_mastfile(mastfile_loc):
+    with open(mastfile_loc, mode='r') as dfile:
+        lines = dfile.readlines()
+    
+    profiles = {}
+    nlines_tot = len(lines)
+    
+    psi_n = np.zeros(nlines_tot)
+    ne = np.zeros(nlines_tot)
+    te = np.zeros(nlines_tot)
+    i = 0
+    
+    while i < nlines_tot:
+        r_line = lines[i].split()
+        psi_n[i] = float(r_line[0])
+        ne[i] = float(r_line[1])*pow(10, -20)
+        te[i] = float(r_line[3])/1000
+        i += 1
+
+    profiles['psi_normal'] = psi_n
+    profiles['electron_density(10^20/m^3)'] = ne
+    profiles['electron_temperature(KeV)'] = te
+    return profiles
+
+
+#------------------------------------------------------------------------------
+
 
 def read_pfile(pfile_loc):
     """
