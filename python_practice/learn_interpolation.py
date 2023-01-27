df32603b27d0fe95355041bb6bef48ba299b3453
yctok <ychuang@wm.edu>
2023-01-27 17:14:19 -0500

    Update learn_interpolation.py

--- python_practice/learn_interpolation.py
+++ python_practice/learn_interpolation.py
@@ -12,8 +12,32 @@ from scipy.interpolate import interp2d
 from scipy.integrate import quad
 from scipy.integrate import solve_ivp
 
+"""
+Purpose of interpolation
+
+Given x_data[...] and y_data[...], we want to create a function y= f(x),
+so we can plug in any value of x and obtain a corresponding value for y
+
+The basic form of interpolation connecting the point in y_data with a straight line,
+then y= f(x) = connecting of straight lines
+(with different starting points and slopes depends on the y_data)
+
+"""
+
 x_data = np.linspace(0,5,5)
 y_data = x_data**2
 
 plt.scatter(x_data, y_data)
 plt.show()
+
+"Picture linear interpolation as a plot"
+
+plt.plot(x_data, y_data, 'o--')
+plt.show()
+
+"make a interpolation function"
+"*y_f is a function, give an x, it will return the interpolate y value"
+
+y_f = interp1d(x_data, y_data, 'linear')
+y_new = y_f(1.5)
+print(y_new)
