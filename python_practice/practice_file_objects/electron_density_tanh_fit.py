6b2b9f9e89375344570a54b6fcc67b06df5ce1a7
yctok <ychuang@wm.edu>
2023-02-27 01:28:39 -0500

    Update electron_density_tanh_fit.py

--- python_practice/practice_file_objects/electron_density_tanh_fit.py
+++ python_practice/practice_file_objects/electron_density_tanh_fit.py
@@ -45,8 +45,11 @@ def read_mastfile(mastfile_loc):
 # def tanh_f(r, b, h, r0, d):
 #     return b+ h*(np.tanh((r0-r)/d))
 
-def tanh_close(x, h, a, m, b, a_2, m_2, b_2):
-    return h+ (a_1)*(np.tanh((m_1)*x+ b_1)) +  m*(r0-r-d)*np.heaviside(r0-r-d, 1)
+# def tanh_fit2(x, h, a_1, m_1, b_1, a_2, m_2, b_2):
+#     return h+ (a_1)*(np.tanh((m_1)*x+ b_1)) + (a_2)*(np.tanh((m_2)*x+ b_2))
+
+def tanh_fit3(x, h, a_1, m_1, b_1, a_2, m_2, b_2, a_3, m_3, b_3):
+    return h+ (a_1)*(np.tanh((m_1)*x+ b_1)) + (a_2)*(np.tanh((m_2)*x+ b_2)) + (a_3)*(np.tanh((m_3)*x+ b_3))
 
 # x_data = np.linspace(0, 1.5, 100)
 # tanh_fit = tanh_f(x_data, 0.2, 0.25, 0.976, 0.1)
@@ -54,19 +57,20 @@ def tanh_close(x, h, a, m, b, a_2, m_2, b_2):
 # plt.plot(x_data, tanh_fit, color='r')
 # plt.show()
 
-p0 = [0.2, 0.2, -33.3, 33.2, 0.05, -33.3, 200]
+p0 = [0.25, 0.131, -93.3, 92.67, 0.0385, -15.2, 10.25, 0.025, -400, 416]
 
 if __name__ == '__main__':
     mast_dat_dict = read_mastfile(args.mastfile_loc)
     psi = mast_dat_dict['psi_normal']
     ne = mast_dat_dict['electron_density(10^20/m^3)']
-    # x_data = np.linspace(0, 1.2, 100)
-    # tanh_fit = tanh_close(x_data, 0.2, 0.2, -33.3, 33.2, 0.05, -33.3, 200)        
-    popt, pcov = curve_fit(tanh_close, psi, ne, p0)
-    print(popt)    
-    tanh_cfit = tanh_close(psi, popt[0], popt[1], popt[2], popt[3],popt[4], popt[5], popt[6])
+    # x_data = np.linspace(0.2, 1.5, 500)
+    # tanh_fit = tanh_fit3(x_data, 0.25, 0.131, -93.3, 92.67, 0.0385, -15.2, 10.25, 0.025, -400, 416)        
+    popt, pcov = curve_fit(tanh_fit3, psi, ne, p0)
+    print(popt)  
+    x_model = np.linspace(min(psi), max(psi), 300)
+    tanh_cfit = tanh_fit3(x_model, popt[0], popt[1], popt[2], popt[3],popt[4], popt[5], popt[6],popt[7], popt[8], popt[9])
     # plt.plot(x_data, tanh_fit, color='r')
-    plt.plot(psi, tanh_cfit, color='r')
+    plt.plot(x_model, tanh_cfit, color='r')
     plt.scatter(psi, ne)
     plt.show()
     
