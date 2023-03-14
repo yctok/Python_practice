af8145e62226328d9b21270879ac78a54699e1dc
yctok <ychuang@wm.edu>
2023-03-13 21:02:56 -0400

    Update SOLPSxport_increment_run.py

--- MAST_xport/SOLPSxport_new/SOLPSxport_increment_run.py
+++ MAST_xport/SOLPSxport_new/SOLPSxport_increment_run.py
@@ -14,6 +14,14 @@ parser = argparse.ArgumentParser(description='Generate new b2.transport.inputfil
 
 parser.add_argument('-g', '--gfileloc', help='location of profs_*.pkl saved profile file', type=str, default=None)
 parser.add_argument('-p', '--profilesloc', help='location of profs_*.pkl saved profile file', type=str, default=None)
+parser.add_argument('-s', '--shotnum', help='shot number; default = None', type=str, default=None)
+parser.add_argument('-t', '--timeid', help='time of profile run; default = None', type=str, default=None)
+parser.add_argument('-r', '--runid', help='profile run id; default = None', type=str, default=None)
+parser.add_argument('-i', '--tiratiofile', help='File location for Ti/TD ratio; default = None', type=str, default=None)
+parser.add_argument('-d', '--tdfileloc', help='File location for TD; default = None', type=str, default=None)
+parser.add_argument('-f', '--fractional_change', help='Fractional change to transport coefficients; default = 1',
+                    type=float, default=1)
+parser.add_argument('-sh', '--shift', help='shift of major radius; default = 1', type=float, default=1)
 args = parser.parse_args()
 
 if __name__ == '__main__':
