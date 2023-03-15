e9cee8b9bc5d8a44936c0a931c2bb6fd230e889d
yctok <ychuang@wm.edu>
2023-03-14 21:53:37 -0400

    Update tanh_fit.py

--- MAST_xport/mit_tanh_fit/tanh_fit.py
+++ MAST_xport/mit_tanh_fit/tanh_fit.py
@@ -11,6 +11,7 @@ import mtanh_fitting as mf
 import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
 from scipy import interpolate
+from scipy.interpolate import interp1d
 
 parser = argparse.ArgumentParser(description='load MAST data file')
 parser.add_argument('-d','--mastfile_loc', type=str, default=None, help='location MAST data file')
@@ -40,16 +41,30 @@ def read_mastfile(mastfile_loc):
     return profiles
 
 if __name__ == '__main__':
+    n_tot = 600
     mast_dat_dict = read_mastfile(args.mastfile_loc)
     psi = mast_dat_dict['psi_normal']
     ne = mast_dat_dict['electron_density(10^20/m^3)']
     te = mast_dat_dict['electron_temperature(KeV)']
     
-    ne_fit = mf.super_fit_osbourne(psi, ne, maxfev=400)
-    ne_superfit = mf.best_osbourne(psi, ne)
+    ne_f = interp1d(psi, ne, kind= 'quadratic')
+    te_f = interp1d(psi, te, kind= 'quadratic')
+    
+    x_model = np.linspace(min(psi), max(psi), n_tot)
+    
+    
+    ne_fit = mf.super_fit_osbourne(x_model, ne_f(x_model))
+    ne_superfit = mf.best_osbourne(x_model, ne_f(x_model))
+    
+    te_fit = mf.super_fit_osbourne(x_model, te_f(x_model))
+    te_superfit = mf.best_osbourne(x_model, te_f(x_model))
+    
+    # te_fit = mf.super_fit_osbourne(psi, te)
+    # te_superfit = mf.best_osbourne(psi, te)
+    
     
     plt.figure(1)
-    plt.plot(psi, ne_fit[0], color='r', label= 'electron density fit')
+    plt.plot(x_model, ne_fit[0], color='r', label= 'electron density fit')
     plt.scatter(psi, ne, label= 'electron density experiment data')
     
     plt.xlabel('Magnetic flux coordinate: ${\psi_N}$', fontdict={"family":"Times New Roman","size": 20})
@@ -57,16 +72,31 @@ if __name__ == '__main__':
     plt.title('Electron density',fontdict={"family":"Times New Roman","size": 20})
     plt.legend()
     
+    
+    
+    plt.figure(2)
+    plt.plot(x_model, te_fit[0], color='r', label= 'electron density fit')
+    plt.scatter(psi, te, label= 'electron density experiment data')
+    
+    plt.xlabel('Magnetic flux coordinate: ${\psi_N}$', fontdict={"family":"Times New Roman","size": 20})
+    plt.ylabel('Electron temperature: ${T_e}$ (eV)', fontdict={"family":"Times New Roman","size": 20})
+    plt.title('Electron temperature',fontdict={"family":"Times New Roman","size": 20})
+    plt.legend()
+
     plt.show()
     
-    # plt.figure(2)
-    # plt.plot(psi, te_fit[0], color='r', label= 'electron density fit')
-    # plt.scatter(psi, te, label= 'electron density experiment data')
-    
-    # plt.xlabel('Magnetic flux coordinate: ${\psi_N}$', fontdict={"family":"Times New Roman","size": 20})
-    # plt.ylabel('Electron temperature: ${T_e}$ (eV)', fontdict={"family":"Times New Roman","size": 20})
-    # plt.title('Electron temperature',fontdict={"family":"Times New Roman","size": 20})
-    # plt.legend()
+    w_datalist = []   
+    for j in range(n_tot):
+        w_list =[]
+        w_list.append("{: .6f}".format(x_model[j]))
+        w_list.append("{: .6f}".format(ne_fit[0][j]))
+        w_list.append("{: .6f}".format(te_fit[0][j]))
+        w_writelist = ' '.join(str(y)+ "\t" for y in w_list)
+        w_datalist.append(w_writelist)
+   
+    with open('fit_27205_275.dat', 'w') as f:
+        for l,w_line in enumerate(w_datalist):   
+            f.writelines(w_line + "\n")
 
     
     
