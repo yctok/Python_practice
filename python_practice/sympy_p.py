f9fadfa75a2dae92d1b05ae6d785d79c11f67576
yctok <ychuang@wm.edu>
2023-04-03 14:59:36 -0400

    Create sympy_p.py

---
+++ python_practice/sympy_p.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 31 14:19:37 2023
+
+@author: Yi-Cheng
+"""
+
+import sympy as sp
+import numpy as np
+import matplotlib as plt
+
+x = sp.symbols('x')
+print(sp.cos(x))
