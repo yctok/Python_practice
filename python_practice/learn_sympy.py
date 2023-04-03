214642f62e6720556eab3ecf0a074772570b8eeb
yctok <ychuang@wm.edu>
2023-04-03 14:59:45 -0400

    Update learn_sympy.py

--- python_practice/learn_sympy.py
+++ python_practice/learn_sympy.py
@@ -7,9 +7,54 @@ Created on Fri Mar 10 14:44:55 2023
 
 "Learn SymPy"
 
-import sympy as smp
+"SymPy, Youtube, by TM Quest"
+
+"Introduction to SymPy"
+
+import sympy as sp
+import math
 import numpy as np
 import matplotlib as plt
 
-x = smp.symbols('x')
-print(smp.cos(x))
+"A symbolic computation system"
+
+# a = sp.sqrt(2)
+# print(a)
+
+# b = sp.sqrt(2)**2
+# print(b)
+
+# c = math.sqrt(2)
+# print(c)
+
+# d = math.sqrt(2)**2
+# print(d)
+
+"Defining Symbols"
+
+"learn how to factor and expand polynomials in SymPy"
+
+"Functions and attributes in this lecture"
+"""
+sp.Symbol(): Defines a new symbol
+sp.symbols(): Defines multiple new symbols
+sp.factor(): Factor an expression (for example a polynomial)
+sp.expand(): Expand and expression
+sp.cos(): The cosine function
+sp.sin(): The sine function
+
+"""
+
+"Make a symbol"
+
+x = sp.Symbol('x')
+print(x)
+
+y = 2*x + 5
+print(y)
+
+z = 2*x + x + 4
+print(z)
+
+
+
