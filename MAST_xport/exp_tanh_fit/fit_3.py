4459cdf93afe8bbe4f6952273e879a14ae7edf0d
yctok <ychuang@wm.edu>
2023-04-18 17:59:17 -0400

    Update fit_3.py

--- MAST_xport/exp_tanh_fit/fit_3.py
+++ MAST_xport/exp_tanh_fit/fit_3.py
@@ -50,19 +50,37 @@ g_loc = glob.glob('{}/{}/{}/yag_*'.format(topdrt, dev, shot))
 print(g_loc)
 
 n_tot = 200
-p0 = [0.97, 0.6, 0.01, -0.3, 3/14]
-p1 = [0.95, 0.2, 0.02, 0, 6/7]
+n_pop = 10
+# solps_psi = 1.09145489
+solps_psi = 1.1
+p0 = [0.97, 0.6, 0.01, -5, 3/14]
+p1 = [0.95, 0.2, 0.02, -5, 6/7]
 
 if __name__ == '__main__':
     mast_dat_dict = tl.read_mastfile(g_loc[-1])
     psi = mast_dat_dict['psi_normal']
     ne = mast_dat_dict['electron_density(10^20/m^3)']
     te = mast_dat_dict['electron_temperature(KeV)']
+    x_pop = np.linspace(max(psi), solps_psi, n_pop)
+    for obj in x_pop:
+        psi = np.append(psi, obj)
+    print(np.where(ne == ne.min()))
+    print(np.where(te == te.min()))
+    ad = np.full(n_pop, 10**(-3))
+    for k in ad:
+        ne = np.append(ne, k)
+    df = np.full(n_pop + 4, 10**(-2))
+    nt = np.zeros(56)
+    for j in range(56):
+        nt[j] = te[j]
+    te = np.append(nt, df)
+        # ne = np.append(ne, k)
+    
     popt_ne, pcov_ne = curve_fit(tl.tanh, psi, ne, p0)
     print(popt_ne)
     popt_te, pcov_te = curve_fit(tl.tanh, psi, te, p1)
     print(popt_te) 
-    x_model = np.linspace(min(psi), max(psi), n_tot)
+    x_model = np.linspace(min(psi), solps_psi, n_tot)
     tanh_ne_fit = tl.tanh(x_model, popt_ne[0], popt_ne[1], popt_ne[2], popt_ne[3], popt_ne[4])
     tanh_te_fit = tl.tanh(x_model, popt_te[0], popt_te[1], popt_te[2], popt_te[3], popt_te[4])
     
@@ -90,13 +108,13 @@ if __name__ == '__main__':
     plt.legend()
     
     
-    plt.figure(3)
-    plt.plot(x_model, gnexp, color='r', label= 'gradiant')
+    # plt.figure(3)
+    # plt.plot(x_model, gnexp, color='r', label= 'gradiant')
     
-    plt.xlabel('Magnetic flux coordinate: ${\psi_N}$', fontdict={"family":"Times New Roman","size": 20})
-    plt.ylabel('Electron density gradiant', fontdict={"family":"Times New Roman","size": 20})
-    plt.title('Electron density gradiant',fontdict={"family":"Times New Roman","size": 20})
-    plt.legend()
+    # plt.xlabel('Magnetic flux coordinate: ${\psi_N}$', fontdict={"family":"Times New Roman","size": 20})
+    # plt.ylabel('Electron density gradiant', fontdict={"family":"Times New Roman","size": 20})
+    # plt.title('Electron density gradiant',fontdict={"family":"Times New Roman","size": 20})
+    # plt.legend()
     
     plt.show()
     
