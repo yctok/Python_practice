a3ef1993d2515d8672aa3e08584494c2c1ec0c0f
yctok <ychuang@wm.edu>
2023-05-03 11:20:50 -0400

    Update tools.py

--- MAST_xport/exp_tanh_fit/tools.py
+++ MAST_xport/exp_tanh_fit/tools.py
@@ -34,6 +34,12 @@ def ex1_number(text):
 
     return [nu, text]
 
+def new_number(text):
+    name = text.split("/",-1)[-2]
+    nu = int(name.split('_')[2])
+
+    return [nu, text]
+
 def unit_dic():
     unit = {
         'ne3da.last10': ['Electron density', 'Electron density: ${n_e}$ (m$^{-3}$)'],
@@ -51,19 +57,26 @@ def read_mastfile(mastfile_loc):
     nlines_tot = len(lines)
     psi_n = np.zeros(nlines_tot)
     ne = np.zeros(nlines_tot)
+    ne_er = np.zeros(nlines_tot)
     te = np.zeros(nlines_tot)
+    te_er = np.zeros(nlines_tot)
+    
     i = 0
     
     while i < nlines_tot:
         r_line = lines[i].split()
         psi_n[i] = float(r_line[0])
         ne[i] = float(r_line[1])*pow(10, -20)
+        ne_er[i] = float(r_line[2])*pow(10, -20)
         te[i] = float(r_line[3])/1000
+        te_er[i] = float(r_line[4])/1000
         i += 1
 
     profiles['psi_normal'] = psi_n
     profiles['electron_density(10^20/m^3)'] = ne
+    profiles['density error(10^20/m^3)'] = ne_er
     profiles['electron_temperature(KeV)'] = te
+    profiles['temperature error(10^20/m^3)'] = te_er
     return profiles
 
 def tanh(r,r0,h,d,b,m):
