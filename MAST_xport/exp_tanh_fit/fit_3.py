beb096b307fcf441dd6063ca3f3e3a263a030060
yctok <ychuang@wm.edu>
2023-05-03 11:20:47 -0400

    Update fit_3.py

--- MAST_xport/exp_tanh_fit/fit_3.py
+++ MAST_xport/exp_tanh_fit/fit_3.py
@@ -11,37 +11,6 @@ from scipy.optimize import curve_fit
 import tools as tl
 import glob
 
-# parser = argparse.ArgumentParser(description='load MAST data file')
-# parser.add_argument('-d','--mastfile_loc', type=str, default=None, help='location MAST data file')
-# args = parser.parse_args()
-
-# def read_mastfile(mastfile_loc):
-#     with open(mastfile_loc, mode='r') as dfile:
-#         lines = dfile.readlines()
-    
-#     profiles = {}
-#     nlines_tot = len(lines)
-#     psi_n = np.zeros(nlines_tot)
-#     ne = np.zeros(nlines_tot)
-#     te = np.zeros(nlines_tot)
-#     i = 0
-    
-#     while i < nlines_tot:
-#         r_line = lines[i].split()
-#         psi_n[i] = float(r_line[0])
-#         ne[i] = float(r_line[1])*pow(10, -20)
-#         te[i] = float(r_line[3])/1000
-#         i += 1
-
-#     profiles['psi_normal'] = psi_n
-#     profiles['electron_density(10^20/m^3)'] = ne
-#     profiles['electron_temperature(KeV)'] = te
-#     return profiles
-
-# def tanh(r, r0, h, d, b, m):
-#     return b+(h/2)*(np.tanh((r0-r)/d)+1) + m*(r0-r-d)*np.heaviside(r0-r-d, 1)
-
-
 basedrt, topdrt = tl.set_wdir()
 dev = 'mast'
 shot = '027205'
@@ -50,7 +19,6 @@ g_loc = glob.glob('{}/{}/{}/yag_*'.format(topdrt, dev, shot))
 print(g_loc)
 
 n_tot = 200
-n_pop = 10
 # solps_psi = 1.09145489
 solps_psi = 1.1
 p0 = [0.97, 0.6, 0.01, -5, 3/14]
@@ -60,21 +28,9 @@ if __name__ == '__main__':
     mast_dat_dict = tl.read_mastfile(g_loc[-1])
     psi = mast_dat_dict['psi_normal']
     ne = mast_dat_dict['electron_density(10^20/m^3)']
+    ne_er = mast_dat_dict['density error(10^20/m^3)']
     te = mast_dat_dict['electron_temperature(KeV)']
-    x_pop = np.linspace(max(psi), solps_psi, n_pop)
-    for obj in x_pop:
-        psi = np.append(psi, obj)
-    print(np.where(ne == ne.min()))
-    print(np.where(te == te.min()))
-    ad = np.full(n_pop, 10**(-3))
-    for k in ad:
-        ne = np.append(ne, k)
-    df = np.full(n_pop + 4, 10**(-2))
-    nt = np.zeros(56)
-    for j in range(56):
-        nt[j] = te[j]
-    te = np.append(nt, df)
-        # ne = np.append(ne, k)
+    te_er = mast_dat_dict['temperature error(10^20/m^3)']
     
     popt_ne, pcov_ne = curve_fit(tl.tanh, psi, ne, p0)
     print(popt_ne)
@@ -86,35 +42,27 @@ if __name__ == '__main__':
     
     gnexp = np.gradient(tanh_ne_fit)
     
-    
     plt.figure(1)
     plt.plot(x_model, tanh_ne_fit, color='r', label= 'electron density fit')
-    plt.scatter(psi, ne, label= 'electron density experiment data')
+    plt.errorbar(psi, ne, ne_er,fmt="o", label= 'electron density experiment data')
     
-    plt.xlabel('Magnetic flux coordinate: ${\psi_N}$', fontdict={"family":"Times New Roman","size": 20})
-    plt.ylabel('Electron density: ${n_e}$ (10$^{20}$*m$^{-3}$)', fontdict={"family":"Times New Roman","size": 20})
-    plt.title('Electron density',fontdict={"family":"Times New Roman","size": 20})
+    plt.xlabel('Magnetic flux coordinate: ${\psi_N}$', fontdict={"family":"Calibri","size": 20})
+    plt.ylabel('Electron density: ${n_e}$ (10$^{20}$*m$^{-3}$)', fontdict={"family":"Calibri","size": 20})
+    plt.title('Electron density',fontdict={"family":"Calibri","size": 20})
     plt.legend()
     
     
     
     plt.figure(2)
     plt.plot(x_model, tanh_te_fit, color='r', label= 'electron temperature fit')
-    plt.scatter(psi, te, label= 'electron temperature experiment data')
+    plt.errorbar(psi, te, te_er, fmt= "o", label= 'electron temperature experiment data')
     
-    plt.xlabel('Magnetic flux coordinate: ${\psi_N}$', fontdict={"family":"Times New Roman","size": 20})
-    plt.ylabel('Electron temperature: ${T_e}$ (KeV)', fontdict={"family":"Times New Roman","size": 20})
-    plt.title('Electron temperature',fontdict={"family":"Times New Roman","size": 20})
+    plt.xlabel('Magnetic flux coordinate: ${\psi_N}$', fontdict={"family":"Calibri","size": 20})
+    plt.ylabel('Electron temperature: ${T_e}$ (KeV)', fontdict={"family":"Calibri","size": 20})
+    plt.title('Electron temperature',fontdict={"family":"Calibri","size": 20})
     plt.legend()
     
     
-    # plt.figure(3)
-    # plt.plot(x_model, gnexp, color='r', label= 'gradiant')
-    
-    # plt.xlabel('Magnetic flux coordinate: ${\psi_N}$', fontdict={"family":"Times New Roman","size": 20})
-    # plt.ylabel('Electron density gradiant', fontdict={"family":"Times New Roman","size": 20})
-    # plt.title('Electron density gradiant',fontdict={"family":"Times New Roman","size": 20})
-    # plt.legend()
     
     plt.show()
     
