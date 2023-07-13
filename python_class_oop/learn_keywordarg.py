e45190c9ad0c59a26bed0f233dad6d92486105d3
yctok <ychuang@wm.edu>
2023-07-13 14:07:33 -0400

    Create learn_keywordarg.py

---
+++ python_class_oop/learn_keywordarg.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul 13 12:17:54 2023
+
+@author: user
+"""
+
+# def print_keyword_args(**kwargs):
+# kwargs is a dict of the keyword args passed to the function
+    # for key, value in kwargs.iteritems():
+    #     print "%s = %s" % (key, value)
