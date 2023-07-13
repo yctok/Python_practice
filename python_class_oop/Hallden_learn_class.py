179e043d8b4b8252967dbecee35b76309aadaa29
yctok <ychuang@wm.edu>
2023-07-13 14:07:31 -0400

    Create Hallden_learn_class.py

---
+++ python_class_oop/Hallden_learn_class.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 11 17:09:18 2023
+
+@author: user
+"""
+
+"Example of a class"
+
+"from Youtube Hallden, https://www.youtube.com/watch?v=rJzjDszODTI"
+
+# class Person:
+#     def __init__(self, age, weight, height, first_name, last_name, catch_phrase):
+#         self.age = age
+#         self.weight = weight
+#         self.height = height
+#         self.first_name = first_name
+#         self.last_name = last_name
+#         self.catch_phrase = catch_phrase
+    
+#     def walk(self):
+#         print("Walking ...")
+    
+#     def run(self):
+#         print("Running ...")
+
+# user = Person(28, 70, 176, "Yi-Cheng", "Chuang", "Simple and straight forward")
+# print(user.catch_phrase)
+# user.walk()
+# user.run()
+
+"Secand example, use real object to understand this abstract object-orianted concept"
+
+# class Bottle:
+#     def __init__(self, volume, type_):
+#         self.volume = volume
+#         self.type_ = type_
+    
+#     def pour(self):
+#         print("Pouring ...")
+    
+#     def fill(self):
+#         print("Filling ...")
+
+# Plastic = Bottle(20, "Plastic")
+# print(Plastic.type_)
+# Plastic.pour()
